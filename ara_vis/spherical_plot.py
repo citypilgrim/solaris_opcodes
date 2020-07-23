904e4d99c2ee50f67a25a614dd2c221550a44706
bboytian <94tian@gmail.com>
2020-07-23 15:58:52 +0800

    ara_vis/ visualisation package initialised

---
+++ ara_vis/spherical_plot.py
@@ -0,0 +1,23 @@
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ..globalimports import *
+
+
+# main func
+def main(datad, keyofint):
+    '''
+    Plots out data dictionary on a 3D axis.
+    The dictionary has to have the following keys: 
+
+    Parameters
+        datad (dict): dictionary data
+    '''
+
+
+
+
+# running
+if __name__ == '__main__':
+    main()
