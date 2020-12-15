267fa5228a42f20695359334048485a473d9feb5
bboytian <94tian@gmail.com>
2020-12-15 17:45:55 +0800

    log 20201215

--- horisweep_calicheck/__main__.py
+++ horisweep_calicheck/__main__.py
@@ -2,12 +2,7 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from pymap3d import ned2geodetic, geodetic2ned
-
-from .latlg2ne import svy21
-from . import scan2ara as s2a
-from . import scan_event as se
 
 
 # params
@@ -15,20 +10,13 @@ _D = 3000
 _delr = 15
 _nrbthres = 5
 
-_phibanum = 10                  # number of points for phib plot
-
-
-# sup func
-def _rangetup_func(lidarNEa, rtra, phita, survey21):
-    rangen_ara = lidarNEa[0] + np.cos(phita)[..., None] * rtra # (phi, bins)
-    rangee_ara = lidarNEa[1] + np.sin(phita)[..., None] * rtra
-    rangeltlg_tup = survey21.computeLatLon(rangen_ara, rangee_ara)
-    return rangeltlg_tup
+_psinum = 10                  # number of points for uncertainty in azimuth plot
 
 
 # main func
 def main(
-        lidarNEa, phib, delphib,
+        lidar_lat, lidar_lg, lidar_h,
+        delphib,
         cadastraldir,
         *nrbds
 ):
@@ -38,19 +26,23 @@ def main(
     2. specify the data set used for calibration check
 
     Plots out the horizontal sweep of the nrb against the cadastral map
-    the geojson can be created and trimmed in /.cadastral_trimmer
+    the geojson can be created and trimmed in /.cadastral_trimmer.
+
+    Note that the coordinates provided by nrb_calc are in spherical coordinates,
+    which map x->N, y->W.
+    But here we plot on a 2D-plane, which maps y->N(latitude), x->E(longitude).
+    This is taken care in the coordinate transform of the azimuthal array from
+    nrb_calc. which converts phi to psi, which is the angle starting from the E axis,
+    aka the longitude axis
 
     Parameters
-        lidarNEa (array like): Northing and easting of lidar
-        phib (float): [rad] bearing offset of lidar
+        lidar_lat (float): [deg] lidar latitude
+        lidar_lg (float): [deg] lidar longitude
+        lidar_h (float): [m] lidar heigh above geodetic ellipsoid
         delphib (float): [rad] uncert of phib
         cadastraldir (str): directory of cadastral geojson
         nrbds (list): list of output from product_calc.nrb_calc
     '''
-    # init NE to latlong converter
-    survey21 = svy21.SVY21()        # converts NE to lat long
-    lidarltlg_a = survey21.computeLatLon(*lidarNEa)
-
     # reading files
     sg_df = gpd.read_file(cadastraldir)
 
@@ -73,128 +65,146 @@ def main(
         NRB_tra = nrbd['NRB_tra']
         r_tra = nrbd['r_tra'] * 1000
         r_trm = nrbd['r_trm']
-        phi_ta = -nrbd['phi_ta'] - phib  # inversion of coordinates from nrb_calc
+        phi_ta = nrbd['phi_ta'] - phib  # azimuthal offset for lidar
+        psi_ta = phi_ta + np.pi/2       # psi is the angle inthe 2D projection
+                                        # anti clockwise from E(longitude) axis
 
-        # scan lines
-        point1ne_lst = [lidarNE_a[0]*np.ones_like(phi_ta), # (phi, 2)
-                        lidarNE_a[1]*np.ones_like(phi_ta)]
-        _D = 4000
-        point2ne_lst = [point1ne_lst[0] + _D*np.cos(phi_ta),
-                        point1ne_lst[1] + _D*np.sin(phi_ta)]
-        pointn_ara = np.stack([point1ne_lst[0], point2ne_lst[0]], axis=1) # (phi, 2)
-        pointe_ara = np.stack([point1ne_lst[1], point2ne_lst[1]], axis=1)
-        pointltlg_tup = survey21.computeLatLon(pointn_ara, pointe_ara) # (lt_ta, lg_ta)
+        # scan lines; coverting local NE to lat long, referenced from the lidar
+        # coordinates
+        point1lat_ta = lidar_lat*np.ones_like(psi_ta)
+        point1lg_ta = lidar_lg*np.ones_like(psi_ta)
+        point1n_ta, point1e_ta, point1d_ta = geodetic2ned(
+            point1lat_ta, point1lg_ta, lidar_h,
+            lidar_lat, lidar_lg, lidar_h
+        )
 
-        # thresholding nrb
+        point2n_ta = point1n_ta + _D*np.sin(psi_ta)
+        point2e_ta = point1e_ta + _D*np.cos(psi_ta)
+        point2lat_ta, point2lg_ta, _ = ned2geodetic(
+            point2n_ta, point2e_ta, point1d_ta,
+            lidar_lat, lidar_lg, lidar_h,
+        )
+
+        # threshold for nrb
         r_trm *= NRB_tra > _nrbthres
-        rangen_ara = lidarNE_a[0] + np.cos(phi_ta)[..., None] * r_tra # (phi, bins)
-        rangee_ara = lidarNE_a[1] + np.sin(phi_ta)[..., None] * r_tra
-        rangeltlg_tup = survey21.computeLatLon(rangen_ara, rangee_ara)
 
-        # errorbar for range
-        deln = _delr/2 * np.cos(phi_ta)[..., None] # (phi, 1)
-        dele = _delr/2 * np.sin(phi_ta)[..., None]
-        errrangen_ara = np.stack([rangen_ara-deln, rangen_ara+deln], axis=2) # (phi, bins, 2)
-        errrangee_ara = np.stack([rangee_ara-dele, rangee_ara+dele], axis=2)
-        errrangeltlg_tup = survey21.computeLatLon(errrangen_ara, errrangee_ara)
+        # range (time, bins)
+        rn_tra = point1n_ta[:, None] + r_tra * np.sin(psi_ta)[:, None]
+        re_tra = point1e_ta[:, None] + r_tra * np.cos(psi_ta)[:, None]
+        rlat_tra, rlg_tra, _ = ned2geodetic(
+            rn_tra, re_tra, point1d_ta[:, None],
+            lidar_lat, lidar_lg, lidar_h,
+        )
 
-        # error bar for azimuth
-        errphirangeltlg_l = [
-        _rangetup_func(lidarNE_a, r_tra, phi_ta + dphib, survey21)
-        for dphib in np.linspace(-delphib, delphib, _phibanum)
-        ]
-        errphilt_tr1l = [x[0][..., None] for x in errphirangeltlg_l]
-        errphilg_tr1l = [x[1][..., None] for x in errphirangeltlg_l]
-        errphilt_trna = np.concatenate(errphilt_tr1l, axis=-1)
-        errphilg_trna = np.concatenate(errphilg_tr1l, axis=-1)
+        ## errorbar for range; (time, bins, 2(lower limit, upper limit))
+        deln_ta = _delr/2 * np.sin(psi_ta)
+        dele_ta = _delr/2 * np.cos(psi_ta)
+        delrn_tr2a = np.stack([
+            rn_tra - deln_ta[:, None], rn_tra + deln_ta[:, None]
+        ], axis=2)
+        delre_tr2a = np.stack([
+            re_tra - dele_ta[:, None], re_tra + dele_ta[:, None]
+        ], axis=2)
+        delrlat_tr2a, delrlg_tr2a, _ = ned2geodetic(
+            delrn_tr2a, delre_tr2a, point1d_ta[:, None, None],
+            lidar_lat, lidar_lg, lidar_h,
+        )
+
+        ## error bar for azimuth; (time, bins, _psinum), 'n' represents _psinum
+        delpsi_na = np.linspace(-delphib, delphib, _psinum)
+        delpsi_tna = psi_ta[:, None] + delpsi_na
+        delpsin_trna = point1n_ta[:, None, None] + r_tra[:, :, None] * \
+            np.sin(delpsi_tna)[:, None, :]
+        delpsie_trna = point1e_ta[:, None, None] + r_tra[:, :, None] * \
+            np.sin(delpsi_tna)[:, None, :]
+        delpsilat_trna, delpsilg_trna, _ = ned2geodetic(
+            delpsin_trna, delpsie_trna, point1d_ta[:, None, None],
+            lidar_lat, lidar_lg, lidar_h
+        )
 
         # plotting
 
         ## lidar
-        ax.plot(*lidarltlg_a[::-1], 'ko')
+        ax.plot(lidar_lg, lidar_lat, 'ko')
 
         ## plotting threshold nrb
-        for j in range(len(phi_ta)):
+        for j in range(len(phi_ta)):  # indexing time
 
             # scan lines
-            ax.plot(pointltlg_tup[1][j],  # (2)
-                    pointltlg_tup[0][j],
-                    '-', alpha=0.3, color=color)
+            ax.plot(
+                point1lg_ta[j], point1lat_ta[j],
+                '-', alpha=0.3, color=color
+            )
 
             # nrb threshold points
             r_rm = r_trm[j]             # (bins)
-            ax.plot(rangeltlg_tup[1][j][r_rm], # (bins)
-                    rangeltlg_tup[0][j][r_rm],
-                    'o', color=markercolor, alpha=markeralpha)
+            ax.plot(
+                rlg_tra[j][r_rm], rlat_tra[j][r_rm],
+                'o', color=markercolor, alpha=markeralpha
+            )
 
-            # # azimuth error bars
-            # pointnum = np.sum(r_rm)
-            # rmind_a = np.where(r_rm)[0]
-            # for k in range(pointnum):
-            #     ax.plot(
-            #         errphilg_trna[k, rmind_a[j], :],
-            #         errphilt_trna[k, rmind_a[j], :],
-            #         '-', color='k'
-            #     )
+            # plotting error bars
+            for k in range(np.sum(r_rm)):
 
-            # # range error bars
-            # copolrangeerr_mask = np.stack([r_rm]*2, axis=1) # (bins, 2)
-            # lgerr_tra = errrangeltlg_tup[1][j][copolrangeerr_mask] # (bins>thres * 2)
-            # lterr_tra = errrangeltlg_tup[0][j][copolrangeerr_mask]
-            # for k in range(0, len(lgerr_tra), 2):
-            #     ax.plot(lgerr_tra[k:k+2], lterr_tra[k:k+2],
-            #              '-', color='k')
+                # plotting range error bars
+                delr_r2m = np.stack([r_rm]*2, axis=1)
+                ax.plot(
+                    delrlg_tr2a[j][delr_r2m][k],
+                    delrlat_tr2a[j][delr_r2m][k],
+                    '-', color='k'
+                )
 
-
-    plt.xlabel('Long [deg]')
-    plt.ylabel('Lat [deg]')
-
+                # plotting azimuth error bars
+                delpsi_r2m = np.stack([r_rm]*_psinum, axis=1)
+                ax.plot(
+                    delpsilg_trna[j][delpsi_r2m][k],
+                    delpsilat_trna[j][delpsi_r2m][k],
+                    '-', color='k'
+                )
 
     # showing
+    plt.xlabel('Long [deg]')
+    plt.ylabel('Lat [deg]')
     plt.show()
+
 
 # running
 if __name__ == '__main__':
     # imports
-    import pandas as pd
     from ..file_readwrite import smmpl_reader
-    from ..product_calc.nrb_calc import nrb_calc
-    from ..global_imports.smmpl_opcodes import *
+    from ..product_calc.nrb_calc import main as nrb_calc
+    from ..global_imports.solaris_opcodes import *
+
+    phib = 140.6
 
     nrbd_l = []
     # nrbd_l.append(nrb_calc(
     #     'smmpl_E2', smmpl_reader,
-    #     date=pd.Timestamp('20200304'),
-    #     starttime=LOCTIMEFN('202003040341', UTCINFO),
-    #     endtime=LOCTIMEFN('202003040900', UTCINFO),
-    #     genboo=True,
+    #     starttime=LOCTIMEFN('202003040341', 0),
+    #     endtime=LOCTIMEFN('202003040900', 0),
+    #     angularoffset=phib
     # ))
     nrbd_l.append(nrb_calc(
         'smmpl_E2', smmpl_reader,
-        starttime=LOCTIMEFN('202008280250', UTCINFO),
-        endtime=LOCTIMEFN('202008280350', UTCINFO),
-        genboo=True,
+        starttime=LOCTIMEFN('202008280250', 0),
+        endtime=LOCTIMEFN('202008280350', 0),
+        angularoffset=phib
     ))
     nrbd_l.append(nrb_calc(
         'smmpl_E2', smmpl_reader,
-        starttime=LOCTIMEFN('202009132007', UTCINFO),
-        endtime=LOCTIMEFN('202009132113', UTCINFO),
-        genboo=True,
+        starttime=LOCTIMEFN('202009132007', 0),
+        endtime=LOCTIMEFN('202009132113', 0),
+        angularoffset=phib
     ))
 
-    phi_ta = nrbd_l[0]['phi_ta'] + np.deg2rad(ANGOFFSET)
-    print(np.rad2deg(phi_ta[0]), np.rad2deg(phi_ta[-1]))
-    phi_ta = nrbd_l[1]['phi_ta'] + np.deg2rad(ANGOFFSET)
-    print(np.rad2deg(phi_ta[0]), np.rad2deg(phi_ta[-1]))
-
-    lidarNE_a = [31272.71725, 21106.03025]  # at E2
-    phib = np.deg2rad(0)
+    lidar_lat, lidar_lg = 1.299119, 103.771232  # [deg], at E2
+    lidar_h = 70
     delphib = np.deg2rad(0.4)
     cadastral_dir = '/home/tianli/SOLAR_EMA_project/codes/smmpl_codes/horisweep_plot/cadastral_trimmer/cadastral_AOI.geojson'
 
     main(
-        lidarNE_a,
-        phib, delphib,
+        lidar_lat, lidar_lg, lidar_h,
+        delphib,
         cadastral_dir,
         *nrbd_l
     )
